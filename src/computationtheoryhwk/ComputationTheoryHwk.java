0670db2a7e59d87ecdc6b2f693539cfdd5880868
Robert Smith <bobbyx140x@gmail.com>
2018-03-13 13:22:04 -0400

    Done

--- src/computationtheoryhwk/ComputationTheoryHwk.java
+++ src/computationtheoryhwk/ComputationTheoryHwk.java
@@ -18,70 +18,100 @@ public class ComputationTheoryHwk {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Lazy way to handle bad input and inccorect DFAs
+        try{
+            // while the user has not input 0 continue to ask them for state transitions
+            while(true){
 
-        while(true){
-         
-            System.out.println("Enter state symbol state ex. 10 1 14)");
+                System.out.println("Enter state symbol state (ex. 10 1 14)");
+                System.out.println("Enter 0 to be done");
+                Scanner scanner = new Scanner(System.in);
+                String line = scanner.nextLine();
+
+                if(line.equals("0")){
+                    break;
+                }else{
+                    
+                    //Takes the input and slits it up
+                    String[] splitString = line.split("\\s");
+                    int from = Integer.valueOf(splitString[0]);
+                    int symbol = Integer.valueOf(splitString[1]);
+                    int to = Integer.valueOf(splitString[2]);
+                    
+                    //Gets the node that the transition is coming from
+                    Node fromNode = Node.getNode(from);
+                    
+                    //This sets the 0 or 1 input node based on input
+                    if(symbol == 0){
+                        fromNode.setZeroInput(to);
+                    }else if(symbol == 1){
+                        fromNode.setOneInput(to);
+                    }
+                    
+                    //Visual representation of what the transition is
+                    System.out.println(fromNode.getState() + "->" + symbol + "->" + to);
+                    System.out.println();
+                }
+            }
+        
+
+            System.out.println("Enter All Accepting states with spaces (ex. 1 3 5 where states 1,3,5 are accpeting states)");
+            System.out.println("Enter 0 to be done");
             Scanner scanner = new Scanner(System.in);
-            String line = scanner.nextLine();
+
+            String input = scanner.nextLine();
             
-            if(line.equals("done")){
-                break;
+            //Splits the string based on spaces
+            String[] acceptingStates = input.split("\\s");
+
+            
+            //Sets all accepting states based on input
+            for(int i = 0; i < acceptingStates.length; i++){
+                String state = acceptingStates[i];
+                int numState = Integer.valueOf(state);
+
+                Node node = Node.getNode(numState);
+
+                if(node != null){
+                    node.setAcceptingState(true);
+                }
             }
             
-            String[] splitString = line.split("\\s");
-            int from = Integer.valueOf(splitString[0]);
-            int symbol = Integer.valueOf(splitString[1]);
-            int to = Integer.valueOf(splitString[2]);
+            //Prints out the accepting states
+            Node.printAcceptingStates();
+
+            System.out.println("Enter a input string of 1s and 0s");
+            Scanner sc = new Scanner(System.in);
+            String inputArray = sc.nextLine();
+
+            //Sets the first Node to the currentNode
+            Node currentNode = Node.getFirstInputNode();
             
-            Node fromNode = Node.getNode(from);
-            
-            if(symbol == 0){
-                fromNode.setZeroInput(to);
-            }else if(symbol == 1){
-                fromNode.setOneInput(to);
+            //Walks through each input and swaps the currentNode based on the input
+            for(int i = 0; i < inputArray.length(); i++){
+                char currentChar = inputArray.charAt(i);
+
+                switch(currentChar){
+                    case '0': 
+                        //System.out.println("going from " + currentNode.toString() + " to " + currentNode.getZeroInput());
+                        currentNode = currentNode.getZeroInput();
+                    break;
+                    case '1':
+                         //System.out.println("going from " + currentNode.toString() + " to " + currentNode.getOneInput());
+                        currentNode = currentNode.getOneInput();
+                    break;       
+                }
             }
-            System.out.println(fromNode.getState() + "->" + symbol + "->" + to);
-            System.out.println();
-        }
-        
-        while(true){
-            System.out.println("Enter an Accepting state or 0 to be done");
-            Scanner scanner = new Scanner(System.in);
-            int state = Integer.valueOf(scanner.nextLine());
             
-            if(state == 0){
-                break;
-            }
-            
-            Node node = Node.getNode(state);
-            node.setAcceptingState(true);
+            //Prints what state the input ends at and if its accepting or not
+            System.out.println("Ending at State " + currentNode.toString());
+        }catch(Exception e){
+            System.out.println("An error has occured. Your input is either bad or you left an empty transition on Your DFA. please try again.");
         }
         
-        System.out.println("Enter a input string of 1s and 0s");
-        Scanner scanner = new Scanner(System.in);
-        char[] charArray = scanner.nextLine().toCharArray();
         
-        
-        Node currentNode = Node.getFirstInputNode();
-        
-        for(int i = 0; i < charArray.length; i++){
-            int currentInput = Integer.valueOf(charArray[i]);
-            
-            switch(currentInput){
-                case 0: currentNode = currentNode.getZeroInput();
-                break;
-                case 1: currentNode = currentNode.getOneInput();
-                break;       
-            }
-        }
-        
-        if(currentNode.isAcceptingState()){
-            System.out.println("ACCEPTING");
-        }else{
-            System.out.println("NON-ACCEPTING");
-        }
         
     }
-        
+    
 }
--- src/computationtheoryhwk/Node.java
+++ src/computationtheoryhwk/Node.java
@@ -13,23 +13,53 @@ import java.util.ArrayList;
  */
 public class Node {
     
-    static public ArrayList<Node> NODES = new ArrayList<Node>();
+    //Holds all instances of Nodes in program
+    static public ArrayList<Node> NODES = new ArrayList<>();
     
+    //Denotes if Node is an accepting state
     private boolean acceptingState;
-    private int state;
-    private Node inputOne;
-    private Node inputZero;
     
+    //the Nodes state
+    private int state;
+    
+    //what node to transition to when 1 is input
+    private Node inputOne;
+    
+    //what node to transition to when 0 is input
+    private Node inputZero;
+ 
+    /*
+    -nodes can only be constructed within this class
+    -every time a node is created it is added to the static NODES list
+    */
     private Node(int state){
         this.state = state;
         this.acceptingState = false;
         Node.NODES.add(this);
     }
     
+    /*
+    Prints all nodes that are in the accepting state
+    */
+    public static void printAcceptingStates(){
+        for(Node node: Node.NODES){
+            if(node.isAcceptingState()){
+                System.out.println(node.state + " is Accepting");
+            }
+        }
+    }
+    
+    /*
+    gets the first input node, where the DFA starts
+    */
     public static Node getFirstInputNode(){
         return Node.NODES.get(0);
     }
     
+    /*
+    gets the node with the specified state, if that node does not exist it is
+    created
+    */
     public static Node getNode(int state){
              
         Node node = null;
@@ -45,17 +75,27 @@ public class Node {
         return node;
     }
     
+    /*
+    Sets the nodes transition when the input is 1
+    */
     public void setOneInput(int state){
        this.inputOne = Node.getNode(state);
     }
     
+        /*
+    Sets the nodes transition when the input is 0
+    */
     public void setZeroInput(int state){
         this.inputZero = Node.getNode(state);
     }
     
+    /*
+    returns if the state is accepting or not
+    */
     public boolean isAcceptingState(){
         return this.acceptingState;
     }
+    
     
     public int getState(){
         return this.state;
@@ -68,10 +108,16 @@ public class Node {
     public Node getOneInput(){
         return this.inputOne;
     }
-    
+    /*
+    sets the node to accepting or non-accepting
+    */
     public void setAcceptingState(boolean accept){
         this.acceptingState = accept;
     }
     
+    @Override
+    public String toString(){
+        return (this.acceptingState) ? String.valueOf(this.state) + "[ACCEPTING]" : (String.valueOf(this.state) + "[NON-ACCEPTING]"); 
+    }
 }
 
